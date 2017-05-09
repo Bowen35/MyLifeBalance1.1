7f79a4f65348260dac994d310b65bb65cd098e3c
rnmstyles <iammcmillan@gmail.com>
2017-05-09 13:46:03 +1000

    added image icons

--- app/src/main/java/com/inducesmile/androidmultiquiz/helper/Helper.java
+++ app/src/main/java/com/inducesmile/androidmultiquiz/helper/Helper.java
@@ -2,10 +2,10 @@ package com.inducesmile.androidmultiquiz.helper;
 
 public class Helper {
 
-    public static final String instruction = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. " +
-            "The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. " +
-            "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites " +
-            "still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like";
+    public static final String instruction = "The My Life Balance application is an application that will help the My Life Balance find out more details of the client and personalise a profile of what they are wanting as a result in life coaching and with what they will be purchasing on the site." + " The Application will ask a series of questions and you will need to select your best answer."+
+
+    "\n\nMy Life Balance, Step by Step Instructions:"+"\n1. When the application starts you will find categories of quiz."+"\n2. Select the category."+"\n3. Select you answer to the quiz question,"+" Think carefully about your response as you cannot go back."+"\n4. Select your overall score to the quiz.";
+
 
     public static final String SHARED_PREF = "share_pref";
 
